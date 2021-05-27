ab403026378cd3045efe9efcd319db8e11ccf1b9
Mei-chan <mei-chan@neko-chxn.xyz>
2021-05-27 21:21:48 +0200

    Preview

--- MeiPassword/UI_Management/Settings.xaml.cs
+++ MeiPassword/UI_Management/Settings.xaml.cs
@@ -16,6 +16,7 @@ using MeiPassword.Algorythmen;
 using MeiPassword.ConfigsSystem;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using ModernMessageBoxLib;
+using MeiPassword.UI_Management;
 
 namespace MeiPassword.UI_Management
 {
@@ -62,11 +63,12 @@ namespace MeiPassword.UI_Management
             try
             {
                 Process.Start("notepad.exe", PathFinding.CONFIGFILE);
-            } catch(Exception x)
+            }
+            catch (Exception x)
             {
                 QModernMessageBox.Show($"Error: \n{x}", "Application Error", QModernMessageBox.QModernMessageBoxButtons.Ok, ModernMessageboxIcons.Info);
             }
-         
+
         }
 
         private void OpenFolder_Click(object sender, RoutedEventArgs e)
@@ -80,17 +82,17 @@ namespace MeiPassword.UI_Management
             bool check = Boolean.Parse(Music_OnOff.IsChecked.ToString().ToLower());
             if (check == false)
             {
-                    try
-                    {
-                        MyIni.DeleteKey("Music", "Audio");
-                        MyIni.Write("Music", "false", "Audio");
-                        BackgroundSysteme.MusicSysteme.play(false);
-            
-                    }
-                    catch (Exception x)
-                    {
-                        QModernMessageBox.Show($"Error: \n{x}", "Application Error", QModernMessageBox.QModernMessageBoxButtons.Ok, ModernMessageboxIcons.Info);
-                    }
+                try
+                {
+                    MyIni.DeleteKey("Music", "Audio");
+                    MyIni.Write("Music", "false", "Audio");
+                    BackgroundSysteme.MusicSysteme.play(false);
+
+                }
+                catch (Exception x)
+                {
+                    QModernMessageBox.Show($"Error: \n{x}", "Application Error", QModernMessageBox.QModernMessageBoxButtons.Ok, ModernMessageboxIcons.Info);
+                }
                 return;
             }
             if (check)
@@ -117,7 +119,7 @@ namespace MeiPassword.UI_Management
         private void Disable_Enable_Discord_Checked(object sender, RoutedEventArgs e)
         {
             var MyIni = new IniFile(PathFinding.CONFIGFILE);
-           
+
             if (Disable_Enable_Discord.IsChecked == true)
             {
                 string edsd = MyIni.Read("DiscordRPC", "System");
@@ -127,7 +129,8 @@ namespace MeiPassword.UI_Management
                     MyIni.Write("DiscordRPC", "true", "System");
                     Disable_Enable_Discord.IsChecked = true;
                 }
-            } else
+            }
+            else
             {
                 string edsd = MyIni.Read("DiscordRPC", "System");
                 bool Discord = Boolean.Parse(edsd);
@@ -136,7 +139,7 @@ namespace MeiPassword.UI_Management
                     MyIni.Write("DiscordRPC", "false", "System");
                     Disable_Enable_Discord.IsChecked = false;
                 }
-               
+
             }
         }
 
@@ -171,7 +174,7 @@ namespace MeiPassword.UI_Management
         private void DiscordRPCCheck(bool discord)
         {
             if (discord == false) Disable_Enable_Discord.IsChecked = false;
-            if (discord == true)  Disable_Enable_Discord.IsChecked = true;
+            if (discord == true) Disable_Enable_Discord.IsChecked = true;
         }
         private void MusicSelection(bool b)
         {
@@ -223,7 +226,7 @@ namespace MeiPassword.UI_Management
         }
         private void DisableAudio()
         {
-          
+
             var MyIni = new IniFile(PathFinding.CONFIGFILE);
             bool b = Boolean.Parse(MyIni.Read("Music", "Audio").ToString());
             if (b)
@@ -238,7 +241,7 @@ namespace MeiPassword.UI_Management
                 {
                     QModernMessageBox.Show($"Error: \n{x}", "Application Error", QModernMessageBox.QModernMessageBoxButtons.Ok, ModernMessageboxIcons.Info);
                 }
-             
+
             }
             return;
         }
@@ -279,7 +282,7 @@ namespace MeiPassword.UI_Management
             color.Content = "Farbe als HTML";
         }
 
-      
+
 
         private void saveset_Click(object sender, RoutedEventArgs e)
         {
@@ -288,7 +291,8 @@ namespace MeiPassword.UI_Management
             if (b.Contains("Englisch"))
             {
                 MyIni.Write("Lang", "1", "System");
-            } else
+            }
+            else
             {
                 MyIni.Write("Lang", "0", "System");
             }
